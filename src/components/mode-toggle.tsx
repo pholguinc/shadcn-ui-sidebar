aa67666a8382c57d2796fc0b1d9362064822dec1
Pedro Holguin <holguinpedro90@gmail.com>
2024-08-06 03:44:22 -0500

    :fire: Agregando plantilla

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -23,7 +23,7 @@ export function ModeToggle() {
             className="rounded-full w-8 h-8 bg-background"
             variant="outline"
             size="icon"
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
           >
             <SunIcon className="w-[1.2rem] h-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
             <MoonIcon className="absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-1000 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
