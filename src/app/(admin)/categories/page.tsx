6e2729c19531299f293a8b1b560372acf8179116
Pedro Holguin <holguinpedro90@gmail.com>
2024-08-06 03:46:12 -0500

    Agregando Planrtilla

--- src/app/(admin)/categories/page.tsx
+++ src/app/(admin)/categories/page.tsx
@@ -13,7 +13,7 @@ import {
 
 export default function CategoriesPage() {
   return (
-    <ContentLayout title="Categories">
+    <ContentLayout title="Categoriess">
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
